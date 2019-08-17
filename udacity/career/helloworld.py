d8a668a48e43192366515e413af6609a6ea11dd9
JennEYoon <jenneyoon@gmail.com>
2019-08-17 11:38:00 -0400

    Readd udacity folder, without git repo folder

---
+++ udacity/career/helloworld.py
@@ -0,0 +1,11 @@
+# File: Testing file, "helloworld.py".
+# Path: root.
+# Date: 9/15/2018 by Jennifer E. Yoon.
+
+print("Hello World!)
+x = 3+5
+print("3+5=", x)
+
+import datetime
+print(datetime)
+print(datatime.datetime.now())
